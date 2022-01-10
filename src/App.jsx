e1fd30a615446ff5eaade154210b8d744d2b6f5b
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-10 17:39:39 +0100

    challenge begin

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,93 @@ function App () {
 
 
   // #region 'APP control and conditional flow'
-    let filteredPosts = posts
+  let filteredPosts = posts
+
+  function addCommentFromForm(formParam, formValueParam) { //removed formparam
+
+    let objectCommentsAdd = {
+        id: state.comments.length += 1,
+        content: formValueParam,
+        imageId: formParam.id
+    }
+
+    formParam.comments.push(objectCommentsAdd)
+    // state.comments.push(objectCommentsAdd) 
+
+    addCommentUpdateToServer(objectCommentsAdd) //this calls the function to update the server
+
+  }
+
+  function addItemFromFormToState(inputParam1, inputParam2, inputParam3, inputParam4) {
+
+    // let idValue = state.images.length + 1
+    let objectItemImages = {
+        // id: state.images.length += 1,
+        id: state.images.length + 1,
+        title: inputParam1,
+        likes: inputParam2,
+        image: inputParam4,
+        comments: [
+            {
+            id: state.comments.length += 1,
+            content: inputParam3,
+            imageId: state.images[state.images.length - 1].id + 1
+            },
+        ]
+    }
+
+    //variable pushed wich is the user form input new item
+    state.images.push(objectItemImages)
+
+    //we also push it to this array comments in the state object
+    let objectItemComments = {
+        id: state.comments.length += 1,
+        content: inputParam3,
+        imageId: state.images[state.images.length - 1].id + 1
+    }
+
+    // state.comments.push(objectItemComments)
+
+    //updating the server
+    addItemFromFormToServer(objectItemImages)
+
+  }
+
+  function addItemFromFormToServer(imagesObjectParam) {
+
+    fetch('http://localhost:3000/images', {
+
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(imagesObjectParam)
+
+    })
+
+  }
+
+  function addCommentUpdateToServer(commentsParam) {
+
+    // for (const element of commentsArrayParam) {
+
+        fetch('http://localhost:3000/comments', {
+
+            method: 'POST',
+
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(commentsParam)
+
+        })
+
+    // }
+
+  }
   // #endregion
 
 
